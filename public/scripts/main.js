54511cf0d1d8c4e5d6590d11380a1e329875ad14
wbroberts <willbarronroberts@gmail.com>
2018-06-17 23:45:33 -0600

    updated ajax calls

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,7 +4,7 @@ const list = document.querySelector('.todos-list');
 
 document.querySelector('#todoForm').addEventListener('submit', e => {
   e.preventDefault();
-  axios.post('http://localhost:3000/todos', {
+  axios.post('./todos', {
     text: e.target[0].value
   })
   e.target[0].value = '';
