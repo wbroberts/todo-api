ea2ea4b8a6c67e8e7335a9f53d8ac85feabe62cc
wbroberts <willbarronroberts@gmail.com>
2018-06-20 08:45:16 -0600

    added env variables

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const { mongoose } = require('./db/mongoose');
 // Routes for db
 const userRoute = require('./api/users');
 const todoRoute = require('./api/todos');
+const homeRoute = require('./api/home');
 
 app.use(express.static(__dirname + './../public'));
 
@@ -20,9 +21,7 @@ app.use(express.json());
 app.use('/users', userRoute);
 app.use('/todos', todoRoute);
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+app.use('/', homeRoute);
 
 // This is the actual server running
 app.listen(port, () => {
